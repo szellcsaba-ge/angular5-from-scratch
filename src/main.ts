e9b46046de41f8da93c0c9583e8d1affe5d97d56
deejayy <deejayy@deejayy.hu>
2018-04-29 20:01:26 +0200

    Reorganize files

--- src/main.ts
+++ src/main.ts
@@ -1,30 +1,6 @@
-import { NgModule, Component }    from '@angular/core';
-import { BrowserModule }          from '@angular/platform-browser';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import { AppModule } from './app/app.module';
 
-@Component({
-  selector: 'app-root',
-  template: 'hello {{ title }}'
-})
-export class AppComponent {
-  title = 'world';
-}
-
-@NgModule({
-  declarations: [
-    AppComponent,
-  ],
-  bootstrap: [
-    AppComponent,
-  ],
-  imports: [
-    BrowserModule,
-  ],
-  providers: [
-    BrowserModule,
-  ]
-})
-export class AppModule {}
-
-platformBrowserDynamic().bootstrapModule(AppModule).catch(err => console.log(err));
-
+platformBrowserDynamic()
+  .bootstrapModule(AppModule)
+  .catch(err => console.log(err));
