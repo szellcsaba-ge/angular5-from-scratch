e046ccf77500b3db35d49d59aab2e2c41b54697e
deejayy <deejayy@deejayy.hu>
2018-04-29 19:02:39 +0200

    Hello world!

---
+++ README.md
@@ -0,0 +1,27 @@
+# Set up an Angular 5 project from scratch
+
+## Measures
+
+### Basic typescript + webpack
+
+"Hello world"
+
+Serve + dev = 331 KB
+Serve + prod = 120 KB
+Dist + prod = 300 byte
+
+### Angular core
+
+Defined a single NgModule which does nothing
+
+Serve + dev = 1.1 MB
+Serve + prod = 276 KB
+Dist + prod = 140 KB
+
+### Angular basic
+
+A simple angular app, which display a hello world from template + property
+
+Serve + dev = 2.8 MB
+Serve + prod = 774 KB
+Dist + prod = 650 KB (incl. zone) -> gzip = 160 KB
--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,30 @@
-console.log("hello world!");
+import { NgModule, Component }    from '@angular/core';
+import { BrowserModule }          from '@angular/platform-browser';
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+
+@Component({
+  selector: 'app-root',
+  template: 'hello {{ title }}'
+})
+export class AppComponent {
+  title = 'world';
+}
+
+@NgModule({
+  declarations: [
+    AppComponent,
+  ],
+  bootstrap: [
+    AppComponent,
+  ],
+  imports: [
+    BrowserModule,
+  ],
+  providers: [
+    BrowserModule,
+  ]
+})
+export class AppModule {}
+
+platformBrowserDynamic().bootstrapModule(AppModule).catch(err => console.log(err));
+
